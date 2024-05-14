1f81916c337d12b8dbaa3af516c432db4864e9e6
Mohammad Zunayed <mdzunayedwd@gmail.com>
2024-05-14 16:11:17 +0600

    filter system added in applied page

--- src/Pages/Home/AppliedJobs/AppliedJobs.jsx
+++ src/Pages/Home/AppliedJobs/AppliedJobs.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import AppliedTable from './AppliedTable';
+import { FaAngleDown } from 'react-icons/fa';
 
 const AppliedJobs = () => {
     const [loadedDatas , setLoadedDatas] = useState([])
+    const [filteredDatas , setFilteredDatas] = useState([])
 
     const user1 = localStorage.getItem('signedUser');
     const signedUser = JSON.parse(user1);
@@ -21,17 +23,58 @@ const AppliedJobs = () => {
         .then(res => res.json())
         .then(data => {
             setLoadedDatas(data)
+            setFilteredDatas(data)
         })
         .catch(error => {
             console.error(error);
         })
     }, [currentEmail]);
 
+    const handleAllJob = () => {
+        setFilteredDatas(loadedDatas);
+    }
+
+    const handleOnSiteJob = () => {
+        const fitteredJobs = loadedDatas.filter(job => job.job_category === 'On-Site Jobs');
+        setFilteredDatas(fitteredJobs);
+    }
+
+    const handleRemoteJob = () => {
+        const fitteredJobs = loadedDatas.filter(job => job.job_category === 'Remote Jobs');
+        setFilteredDatas(fitteredJobs);
+    }
+
+    const handleHybridJob = () => {
+        const fitteredJobs = loadedDatas.filter(job => job.job_category === 'Hybrid Jobs');
+        setFilteredDatas(fitteredJobs);
+    }
+
+    const handlePartTimeJob = () => {
+        const fitteredJobs = loadedDatas.filter(job => job.job_category === 'Part-Time Jobs');
+        setFilteredDatas(fitteredJobs);
+    }
+
     return (
         <div className="mt-10 w-full lg:w-[1000px] xl:w-[1300px] mx-auto">
             <div>
                 <h2 className="text-4xl text-sky-500 font-bold text-center">Your Applied Jobs</h2>
-                <div className="border-t-2 border-sky-600 w-[300px] mx-auto mt-2 mb-6"></div>
+                <div className="border-t-2 border-sky-600 w-[300px] mx-auto mt-2 mb-4"></div>
+            </div>
+            <div className='text-center mb-4'>
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn m-1 bg-sky-500 hover:bg-sky-500 rounded text-black font-bold flex items-center">Select Category<FaAngleDown className='text-lg'></FaAngleDown> </div>
+                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 w-52 rounded border-2 border-sky-500 font-bold">
+                        <li><a onClick={handleAllJob}>All Jobs</a></li>
+                        <div className='border-t-2 border-sky-400 my-1'></div>
+                        <li><a onClick={handleOnSiteJob}>On-Site Jobs</a></li>
+                        <div className='border-t-2 border-sky-400 my-1'></div>
+                        <li><a onClick={handleRemoteJob}>Remote Jobs</a></li>
+                        <div className='border-t-2 border-sky-400 my-1'></div>
+                        <li><a onClick={handleHybridJob}>Hybrid Jobs</a></li>
+                        <div className='border-t-2 border-sky-400 my-1'></div>
+                        <li><a onClick={handlePartTimeJob}>Part-Time Jobs</a></li>
+                    </ul>
+                </div>
             </div>
             <div className="overflow-x-auto border-b-2 border-sky-500">
                 <table className="table">
@@ -47,12 +90,13 @@ const AppliedJobs = () => {
                         <th className="w-12"></th>
                     </tr>
                     </thead>
-                    <tbody>
+                    { loadedDatas.length === 0 ? <></> : <tbody>
                         {
-                            loadedDatas.map(job => <AppliedTable key={job._id} job={job}></AppliedTable>)
+                            filteredDatas.map(job => <AppliedTable key={job._id} job={job}></AppliedTable>)
                         }
-                    </tbody>
+                    </tbody>}
                 </table>
+                {loadedDatas.length === 0 ? <h2 className='text-4xl text-red-500 font-bold text-center my-8'>You haven't applied any job yet :(</h2> : <></>}
             </div>
         </div>
     );
--- src/Shared/NavbarWeb/NavbarWeb.jsx
+++ src/Shared/NavbarWeb/NavbarWeb.jsx
@@ -92,12 +92,12 @@ const NavbarWeb = () => {
     }
 
     return (
-        <div className="navbar md:px-6 border-b-2 drop-shadow-lg border-sky-600 fixed top-0 left-0 w-full z-[1000] bg-[#ffffffE6] text-black">
+        <div className="navbar md:px-6 border-b-2 drop-shadow-lg border-sky-600 fixed top-0 left-0 w-full z-[1000] bg-[#ffffffE6]">
             <div className="navbar-start">
-                <Link><a className="text-3xl font-bold">Job<span className='text-sky-500'>Wander</span></a></Link>
+                <Link><a className="text-3xl font-bold text-black">Job<span className='text-sky-500'>Wander</span></a></Link>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1 font-bold items-center">
+                <ul className="menu menu-horizontal px-1 font-bold items-center text-black">
                     {navLinks}
                 </ul>
             </div>
@@ -114,7 +114,7 @@ const NavbarWeb = () => {
 
                 { user1 || user2 ? <button onClick={handleSignOut} className='btn rounded bg-red-600 font-bold px-6 text-lg text-black hover:bg-red-500'>SignOut</button> : <Link to={'/login'}><button className='btn rounded bg-sky-500 font-bold px-6 text-lg text-black hover:bg-sky-300'>Login</button></Link>}
 
-                <label className="swap swap-rotate hidden lg:grid border-l-2 border-sky-600 pl-2 py-1">
+                <label className="swap swap-rotate hidden lg:grid border-l-2 border-sky-600 pl-2 py-1 text-black">
                     <input type="checkbox" className="theme-controller hidden" value="dark" />
                     {/* sun icon */}
                     <svg className="swap-off fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
